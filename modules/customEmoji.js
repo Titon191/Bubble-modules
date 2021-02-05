a4fd89e6c5b2611886a5785a80db40c3614e6765
Titon <titondesign.kontakt@gmail.com>
2021-02-05 09:32:21 +0100

    Create customEmoji.js

---
+++ modules/customEmoji.js
@@ -0,0 +1,8 @@
+// customEmoji Module
+const customEmoji = function (emoji) {
+    let customEmoji = ""
+    
+    if (emoji === "example") customEmoji = "<:emoji-name:emoji-id>"
+    
+    return customEmoji
+}
