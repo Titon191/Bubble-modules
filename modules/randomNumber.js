f3e62fadec829a43b1b803e21c589d5779d1ef47
Titon <titondesign.kontakt@gmail.com>
2021-04-09 19:43:17 +0200

    Update randomNumber.js

--- modules/randomNumber.js
+++ modules/randomNumber.js
@@ -1,4 +1,7 @@
-// randomNumber Module
+// RANDOMNUMBER
+// Losuje losowy numer
+// Przykład:
+// Input: randomNumber(1, 10) || Przykładowy output: 7
 const randomNumber = function (min, max) {
     min = Math.ceil(min);
     max = Math.floor(max) + 1;
