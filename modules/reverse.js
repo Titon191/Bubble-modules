c089412105eaeb4d5e53549dbe51739479adf577
Titon <titondesign.kontakt@gmail.com>
2021-04-09 19:37:40 +0200

    Update reverse.js

--- modules/reverse.js
+++ modules/reverse.js
@@ -1,4 +1,6 @@
-// reverse module
+// REVERSE
+// Odwraca podany tekst - przykład:
+// Input: reverse("tekst") || Output: "tsket" 
 const reverse = function (text) {
     return text.split("").reverse().join("");
   }
